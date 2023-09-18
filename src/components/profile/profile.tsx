8064217dd48563bc15fae197a8e38f4bcfde0404
Yutthasit Pukdeeboon <yutthasit_p@Nui-Mac-mini.local>
2023-09-18 22:02:10 +0700

    update 1st version

---
+++ src/components/profile/profile.tsx
@@ -0,0 +1,27 @@
+import React, { FC, ReactElement} from 'react';
+import { Avatar,Box, Typography } from '@mui/material';
+
+export const Profile: FC = (): ReactElement => {
+    return(
+        <Box display="flex" 
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center">
+            <Avatar
+            sx={{
+                width: '96px',
+                height: '96px',
+                backgroundColor: 'primary.main',
+                marginBottom: '16px',
+            }}>
+                <Typography variant="h4" color="text.primary">N</Typography>
+            </Avatar>
+            <Typography variant="h6" color="text.primary">
+                Welcome, Nui
+            </Typography>
+            <Typography variant="body1" color="text.primary">
+                This is your personal task manager
+            </Typography>
+        </Box>
+    );
+}
---
+++ src/components/sidebar/sidebar.tsx
@@ -0,0 +1,24 @@
+import React, { FC, ReactElement} from 'react';
+import { Grid } from '@mui/material';
+import {Profile} from '../profile/profile';
+
+export const Sidebar: FC = (): ReactElement => {
+
+    return (
+        <Grid item md={4} sx={{
+            height: '100vh',
+            position: 'fixed',
+            right: 0,
+            top: 0,
+            width: '100%',
+            backgroundColor: 'background.paper',
+            display: 'flex',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            alignItems: 'center',
+        }}> 
+        <Profile></Profile>
+        </Grid>
+    );
+
+}
